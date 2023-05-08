95b41fdec58a0a75baa74cf4a4e66d87ee0a697a
Poterap <104162233+Poterap@users.noreply.github.com>
2023-05-08 11:38:37 +0200

    poprawiona transakcja z bazą danych i dodanie informacji o modyfikacji volume

--- src/parsers/D2/influx.py
+++ src/parsers/D2/influx.py
@@ -5,29 +5,29 @@ import sys
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-# Ustawienia połączenia z bazą danych
+# Database connection settings
 token = "U2H9TCSHMNdtNaeyt7NwovtLIuKjC3LZRGCUSab6VgK3DWZSHBknt0IfZsCGXkJXWVMH2I8JurcXr0Jw0FWbLQ=="
 org = "my-org"
-bucket = "stooq"
+bucket = "test"
 url = "http://localhost:8086"
 
-# Inicjalizacja klienta bazy danych
+# Initialize InfluxDB client
 client = InfluxDBClient(url=url, token=token)
 
-# Ścieżka do folderu z plikami CSV
+# Path to the folder with CSV files
 folder_path = './data/stooq_data_2023-05-04'
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-# Pobranie listy plików z danego folderu
+# Get the list of files in the folder
 for file_name in os.listdir(folder_path):
-    # Pobranie symbolu i daty z nazwy pliku
+    # Get the symbol and date from the file name
     symbol, date_str = os.path.splitext(file_name)[0].split('_')
     date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
 
     file_path = os.path.join(folder_path, file_name)
 
-    # Otwarcie pliku CSV dla danej spółki
+    # Open the CSV file for the given stock
     with open(file_path) as csvfile:
         reader = csv.DictReader(csvfile)
 
@@ -41,6 +41,14 @@ for file_name in os.listdir(folder_path):
             try:
                 if row['Otwarcie'] and row['Najwyzszy'] and row['Najnizszy'] and row['Zamkniecie']:
                     rows_with_data += 1
+                    volume_modified = False  # flag for volume modification
+                    if row['Wolumen']:
+                        volume_modified = True
+                        volume = float(row['Wolumen'])
+                    else:
+                        volume_modified = False
+                        volume = 0.0
+
                     point = (
                         Point(date)
                         .tag("symbol", symbol)
@@ -49,16 +57,20 @@ for file_name in os.listdir(folder_path):
                         .field("high", float(row['Najwyzszy']))
                         .field("low", float(row['Najnizszy']))
                         .field("close", float(row['Zamkniecie']))
-                        .field("volume", float(row['Wolumen']) if row['Wolumen'] else 0.0)
+                        .field("volume", volume)
                     )
+                    point.modified = {
+                        "volume": volume_modified
+                    }  # Add dictionery of flags to the point
                     points.append(point)
             except Exception as e:
-                print(f"Błąd dla wiersza {rows_processed} {symbol} {row['Data']}: {e}")
+                print(f"Error processing row {rows_processed} for {symbol} {row['Data']}: {e}")
+                break
 
-        # Zapis wszystkich punktów do bazy danych
+        # Write all the points to the database
         try:
             write_api.write(bucket=bucket, org=org, record=points)
         except Exception as e:
-            print(f"Błąd podczas zapisu punktów do bazy danych: {e}")
+            print(f"Error writing points to the database: {e}")
 
-        print(f'{rows_with_data} of {rows_processed} rows have data for {symbol}')
+        print(f'{rows_processed} of {rows_with_data} rows processed for {symbol}')
