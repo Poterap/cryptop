6698f18a69a6f90344d660e28612720bba976865
Poterap <104162233+Poterap@users.noreply.github.com>
2023-05-15 19:25:32 +0200

    ADORK-61-62-63 utworzenei serwisu działajacego w tle i konfiguracja docker-compose requirements i dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
- FROM python:3.9
+FROM python:3.9 as fastapi
  
 WORKDIR /crypto
  
@@ -9,3 +9,16 @@ RUN pip install --no-cache-dir --upgrade -r /crypto/requirements.txt
 COPY ./src /crypto/src
 
 CMD ["uvicorn", "src.app.main:app", "--host", "0.0.0.0", "--port", "80"]
+
+FROM python:3.9 as scheduler
+
+WORKDIR /crypto
+
+COPY ./requirements.txt /crypto/requirements.txt
+
+RUN pip install --no-cache-dir --upgrade -r /crypto/requirements.txt
+
+COPY ./src/executors/eda/script.py /crypto/src/executors/eda/script.py
+COPY ./src/log /crypto/src/log
+
+CMD ["python", "-m", "src.executors.eda.script"]
---
+++ src/executors/eda/script.py
@@ -0,0 +1,28 @@
+import datetime
+import schedule
+import time
+from src.log.logger import My_logger
+from src.config.config_reader import read_config
+
+
+config = read_config()
+
+logger = My_logger(name=config['scheduler']['logger_name'], filename=config['scheduler']['logger_path'])
+
+
+def main():
+
+    schedule.every(config['scheduler']['every_minutes']).minutes.do(run_job)
+
+    while True:
+        schedule.run_pending()
+
+def run_job():
+    current_time = datetime.datetime.now()
+    result = f"Zadanie wykonane o {current_time}"
+    print(result)
+
+    logger.info(result)
+
+if __name__ == "__main__":
+    main()
