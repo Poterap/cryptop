0efddc83e6f07690efc72c639b8cd7cc7f4d3b12
Poterap <104162233+Poterap@users.noreply.github.com>
2023-05-16 22:17:55 +0200

    ADORK-65 transforming many csv and saving them to one file using kedro. fix of fastapi and scheduler configuration for docker

--- kedro-cryptop/src/kedro_cryptop/pipelines/data_prepering/nodes.py
+++ kedro-cryptop/src/kedro_cryptop/pipelines/data_prepering/nodes.py
@@ -1,4 +1,15 @@
+from typing import Callable, Dict
 import pandas as pd
 
-def fill_ticker_nans(ticker_df: pd.DataFrame) -> pd.DataFrame:
-    return ticker_df.fillna(0)
+def fill_ticker_nans(ticker_df: Dict[str, Callable[[], pd.DataFrame]]) -> pd.DataFrame:
+
+    merged_df = pd.DataFrame()
+
+    for file_name, load_func in ticker_df.items():
+        df = load_func()
+        filled_df = df.fillna(0)
+        symbol_date = file_name.split('_')
+        filled_df['symbol'] = symbol_date[0]
+        merged_df = pd.concat([merged_df, filled_df])
+
+    return merged_df
